7e03401d33e09a185660d969a27710166e435487
Sven Strittmatter <ich@weltraumschaf.de>
2019-11-07 11:32:05 +0100

    Rename
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- main.go
+++ main.go
@@ -5,64 +5,25 @@ import (
     "log"
     "os"
     "github.com/urfave/cli"
-    "strings"
 )
 
 var app = cli.NewApp()
 
 // https://itnext.io/how-to-create-your-own-cli-with-golang-3c50727ac608
 // http://jodies.de/ipcalc?host=10.0.0.0&mask1=16&mask2=
-var pizza = []string{"Enjoy your pizza with some delicous"}
-
-func info() {
-    app.Name = "ipcalc"
-    app.Usage = "Tool to calculate IP stuff"
-    app.Author = "Sven Strittmatter"
-    app.Version = "1.0.0"
-}
-
-func commands() {
-    app.Commands = []cli.Command{
-        {
-            Name:    "peppers",
-            Aliases: []string{"p"},
-            Usage:   "Add peppers to your pizza",
-            Action: func(c *cli.Context) {
-                pe := "peppers"
-                peppers := append(pizza, pe)
-                m := strings.Join(peppers, " ")
-                fmt.Println(m)
-            },
-        },
-        {
-            Name:    "pineapple",
-            Aliases: []string{"pa"},
-            Usage:   "Add pineapple to your pizza",
-            Action: func(c *cli.Context) {
-                pa := "pineapple"
-                pineapple := append(pizza, pa)
-                m := strings.Join(pineapple, " ")
-                fmt.Println(m)
-            },
-        },
-        {
-            Name:    "cheese",
-            Aliases: []string{"c"},
-            Usage:   "Add cheese to your pizza",
-            Action: func(c *cli.Context) {
-                ch := "cheese"
-                cheese := append(pizza, ch)
-                m := strings.Join(cheese, " ")
-                fmt.Println(m)
-            },
-        },
-    }
-}
 
 func main() {
-    info()
-    commands()
-
+    app.Name = "calculon"
+    app.Usage = "Tool to calculate IP stuff"
+    app.Author = "Sven Strittmatter"
+    app.Email = "ich@weltraumschaf.de"
+    app.Description = "TODO Description"
+    app.Version = "1.0.0"
+    app.Action = func(c *cli.Context) error {
+        fmt.Println("boom! I say!")
+        return nil
+    }
+    
     err := app.Run(os.Args)
 
     if err != nil {
