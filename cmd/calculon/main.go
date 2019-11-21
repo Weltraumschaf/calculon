486de6786e4782c4ccd02cc8a4fa3704e3afc405
Sven Strittmatter <ich@weltraumschaf.de>
2019-11-21 18:46:17 +0100

    Move everything out of the main function
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- cmd/calculon/main.go
+++ cmd/calculon/main.go
@@ -2,23 +2,12 @@ package main
 
 import (
     "fmt"
-    "github.com/urfave/cli"
     "os"
     "weltraumschaf.de/calculon/internal/calculon"
 )
 
-var app = cli.NewApp()
-
 func main() {
-    app.Name = "calculon"
-    app.Usage = "Tool to calculate IP stuff"
-    app.UsageText = app.Name + " 192.168.123.0/24"
-    app.Author = "Sven Strittmatter"
-    app.Email = "ich@weltraumschaf.de"
-    app.Description = "TODO Description"
-    app.Version = "1.0.0"
-    app.Action = calculon.Execute
-
+    var app = calculon.Create()
     err := app.Run(os.Args)
 
     if err != nil {
--- internal/calculon/app.go
+++ internal/calculon/app.go
@@ -5,6 +5,19 @@ import (
     "github.com/urfave/cli"
 )
 
+func Create() *cli.App {
+    var app = cli.NewApp()
+    app.Name = "calculon"
+    app.Usage = "Tool to calculate IP stuff"
+    app.UsageText = app.Name + " 192.168.123.0/24"
+    app.Author = "Sven Strittmatter"
+    app.Email = "ich@weltraumschaf.de"
+    app.Description = "TODO Description"
+    app.Version = "1.0.0"
+    app.Action = Execute
+    return app
+}
+
 func Execute(c *cli.Context) error {
     fmt.Println("Hello, World!")
     return nil
