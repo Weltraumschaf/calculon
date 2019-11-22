9e82bdc2aff169247a22b28a1c0409434efd28d6
Sven Strittmatter <ich@weltraumschaf.de>
2019-11-22 16:56:52 +0100

    Implement method to format IP adress to bit string
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- internal/calculon/format.go
+++ internal/calculon/format.go
@@ -2,10 +2,22 @@ package calculon
 
 import (
     "fmt"
+    "net"
     "strconv"
+    "strings"
 )
 
-func FormatByteToBitString(input byte) string {
+func FormatByteAsBits(input byte) string {
     binary := strconv.FormatUint(uint64(input), 2)
     return fmt.Sprintf("%08s", binary)
+}
+
+func FormatIpAsDottedBits(ip net.IP) (string, error) {
+    var result []string
+
+    for _, b := range ip.To4() {
+        result = append(result, FormatByteAsBits(b))
+    }
+
+    return strings.Join(result, "."), nil
 }
--- internal/calculon/format_test.go
+++ internal/calculon/format_test.go
@@ -2,22 +2,31 @@ package calculon
 
 import (
     "github.com/stretchr/testify/assert"
+    "net"
     "testing"
 )
 
-func TestFormatByteToBitString(t *testing.T) {
-    assert.Equal(t, "00000000", FormatByteToBitString(0x00))
-    assert.Equal(t, "00000001", FormatByteToBitString(0x01))
-    assert.Equal(t, "00000010", FormatByteToBitString(0x02))
-    assert.Equal(t, "00000011", FormatByteToBitString(0x03))
-    assert.Equal(t, "00000100", FormatByteToBitString(0x04))
-    assert.Equal(t, "00000101", FormatByteToBitString(0x05))
-    assert.Equal(t, "00000110", FormatByteToBitString(0x06))
-    assert.Equal(t, "00000111", FormatByteToBitString(0x07))
-    assert.Equal(t, "00001000", FormatByteToBitString(0x08))
-    assert.Equal(t, "00010000", FormatByteToBitString(0x10))
-    assert.Equal(t, "00100000", FormatByteToBitString(0x20))
-    assert.Equal(t, "01000000", FormatByteToBitString(0x40))
-    assert.Equal(t, "10000000", FormatByteToBitString(0x80))
-    assert.Equal(t, "11111111", FormatByteToBitString(0xFF))
+func TestFormatByteAsBits(t *testing.T) {
+    assert.Equal(t, "00000000", FormatByteAsBits(0x00))
+    assert.Equal(t, "00000001", FormatByteAsBits(0x01))
+    assert.Equal(t, "00000010", FormatByteAsBits(0x02))
+    assert.Equal(t, "00000011", FormatByteAsBits(0x03))
+    assert.Equal(t, "00000100", FormatByteAsBits(0x04))
+    assert.Equal(t, "00000101", FormatByteAsBits(0x05))
+    assert.Equal(t, "00000110", FormatByteAsBits(0x06))
+    assert.Equal(t, "00000111", FormatByteAsBits(0x07))
+    assert.Equal(t, "00001000", FormatByteAsBits(0x08))
+    assert.Equal(t, "00010000", FormatByteAsBits(0x10))
+    assert.Equal(t, "00100000", FormatByteAsBits(0x20))
+    assert.Equal(t, "01000000", FormatByteAsBits(0x40))
+    assert.Equal(t, "10000000", FormatByteAsBits(0x80))
+    assert.Equal(t, "11111111", FormatByteAsBits(0xFF))
+}
+
+func TestFormatIpAsDottedBits(t *testing.T) {
+    result, _ := FormatIpAsDottedBits(net.ParseIP("192.168.1.1"))
+    assert.Equal(t, "11000000.10101000.00000001.00000001", result)
+
+    result, _ = FormatIpAsDottedBits(net.ParseIP("1.2.3.4"))
+    assert.Equal(t, "00000001.00000010.00000011.00000100", result)
 }
