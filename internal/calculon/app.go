2000ba4a85a8ddcd8d81fba622d317c0692c94f0
Sven Strittmatter <ich@weltraumschaf.de>
2019-11-22 09:48:54 +0100

    Move legacy code in app file
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- internal/calculon/app.go
+++ internal/calculon/app.go
@@ -2,8 +2,11 @@ package calculon
 
 import (
     "errors"
+    "fmt"
     "github.com/urfave/cli"
     "net"
+    "strconv"
+    "strings"
 )
 
 func Create() *cli.App {
@@ -32,5 +35,119 @@ func Execute(c *cli.Context) error {
         return errors.New("Invalid netmask given!")
     }
 
-    return PrintResult(ip, network)
+    return printResult(ip, network)
 }
+
+func printResult(ip net.IP, netmask *net.IPNet) error {
+    binaryIp, err := FormatBinary(ip)
+
+    if err != nil {
+        return nil
+    }
+
+    fmt.Printf("Address:   %s       %s\n", ip, binaryIp)
+
+    //binaryNetmask, err := NetmaskToBinary(netmask)
+    //
+    //if err != nil {
+    //    return nil
+    //}
+    //
+    //decimalNetmask, err := NetmaskToOctets(netmask)
+    //
+    //if err != nil {
+    //    return nil
+    //}
+
+    //fmt.Printf("Netmask:   %s = %d  %s\n",
+    //    decimalNetmask, netmask, binaryNetmask)
+    fmt.Printf("Wildcard:  \n")
+    fmt.Printf("Network:   \n")
+    fmt.Printf("Broadcast: \n")
+    fmt.Printf("HostMin:   \n")
+    fmt.Printf("HostMax:   \n")
+    fmt.Printf("Hosts/Net: \n")
+    return nil
+}
+
+func FormatBinary(ip net.IP) (string, error) {
+    var result []string
+
+    for _, octet := range strings.Split(ip.String(), ".") {
+        o, err := strconv.Atoi(octet)
+
+        if err != nil {
+            return "", err
+        }
+
+        binaryOctet := strconv.FormatInt(int64(o), 2)
+        binaryOctet = fmt.Sprintf("%08s", binaryOctet)
+        result = append(result, binaryOctet)
+    }
+
+    return strings.Join(result, "."), nil
+}
+
+func NetmaskToOctets(netmask int) (string, error) {
+    octets, err := NetmaskToBytes(netmask)
+
+    if err != nil {
+        return "", err
+    }
+
+    var result []string
+
+    for _, octet := range octets {
+        binaryOctet := strconv.FormatInt(int64(octet), 10)
+        result = append(result, binaryOctet)
+    }
+
+    return strings.Join(result, "."), nil
+}
+
+func NetmaskToBinary(netmask int) (string, error) {
+    octets, err := NetmaskToBytes(netmask)
+
+    if err != nil {
+        return "", err
+    }
+
+    var result []string
+
+    for _, octet := range octets {
+        binaryOctet := strconv.FormatInt(int64(octet), 2)
+        binaryOctet = fmt.Sprintf("%08s", binaryOctet)
+        result = append(result, binaryOctet)
+    }
+
+    return strings.Join(result, "."), nil
+}
+
+const maxNetmask = 32
+
+func NetmaskToBytes(netmask int) ([]uint64, error) {
+    if netmask < 1 {
+        return nil, errors.New(
+            fmt.Sprintf("Invalid netmask! Value is less than one: %d", netmask))
+    }
+
+    if netmask > maxNetmask {
+        return nil, errors.New(
+            fmt.Sprintf("Invalid netmask! Value is greater than %d: %d", maxNetmask, netmask))
+    }
+
+    var binaryNetmask uint64
+    binaryNetmask = (0xFFFFFFFF << (maxNetmask - netmask)) & 0xFFFFFFFF
+    var countDownBits uint64
+    countDownBits = maxNetmask
+    octets := make([]uint64, 0, 4)
+
+    for i := 1; i <= 4; i++ {
+        tmp := binaryNetmask >> (countDownBits - 8) & 0xFF
+        octets = append(octets, tmp)
+        countDownBits -= 8
+    }
+
+    return octets, nil
+}
+
--- internal/calculon/format_test.go
+++ internal/calculon/format_test.go
@@ -2,60 +2,8 @@ package calculon
 
 import (
     "github.com/stretchr/testify/assert"
-    "net"
     "testing"
 )
-
-func TestFormatBinary(t *testing.T) {
-    result, err := FormatBinary(net.ParseIP("192.168.123.5"))
-
-    assert.Empty(t, err)
-    assert.Equal(t, "11000000.10101000.01111011.00000101", result)
-}
-
-func TestNetmaskToOctets(t *testing.T) {
-    result, err := NetmaskToOctets(24)
-
-    assert.Empty(t, err)
-    assert.Equal(t, "255.255.255.0", result)
-}
-
-func TestNetmaskToBinary(t *testing.T) {
-    result, err := NetmaskToBinary(24)
-
-    assert.Empty(t, err)
-    assert.Equal(t, "11111111.11111111.11111111.00000000", result)
-}
-
-func TestNetmaskToBytes(t *testing.T) {
-    result, err := NetmaskToBytes(24)
-
-    assert.Empty(t, err)
-    assert.Equal(t, []uint64{0xFF, 0xFF, 0xFF, 0x00}, result)
-}
-
-func TestNetmaskToBytes_netmaskToSmall(t *testing.T) {
-    result, err := NetmaskToBytes(0)
-
-    assert.Nil(t, result)
-    assert.NotNil(t, err)
-}
-
-func TestNetmaskToBytes_netmaskToBig(t *testing.T) {
-    result, err := NetmaskToBytes(33)
-
-    assert.Nil(t, result)
-    assert.NotNil(t, err)
-}
-
-func TestFoo(t *testing.T) {
-    ip, network, _ := net.ParseCIDR("192.168.123.5/24")
-    bytes, _ := ip.To4().MarshalText()
-
-    assert.Equal(t, nil, bytes)
-    assert.Equal(t, "", ip.String())
-    assert.Equal(t, "", network.String())
-}
 
 func TestFormatByteToBitString(t *testing.T) {
     assert.Equal(t, "00000000", FormatByteToBitString(0x00))
